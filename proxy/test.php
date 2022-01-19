47bd58db0e1a2f0b0b02cbe2f185a6ce9feaa390
zyz <zyz@zhaoyingze.com>
2022-01-20 00:06:33 +0800

    add port test

--- proxy/test.php
+++ proxy/test.php
@@ -2,5 +2,6 @@
 echo $_SERVER["HTTPS"];
 echo isset($_SERVER["HTTPS"]);
 echo $_SERVER["SERVER_PORT"];
-echo $_SERVER["SERVER_PORT"] === 443
+echo $_SERVER["SERVER_PORT"] === 443;
+echo $_SERVER["SERVER_PORT"] == 443;
 ?>
