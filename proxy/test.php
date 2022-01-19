1a45b83b7d8402de6792c6be4d8d54badd1d1ec2
zyz <zyz@zhaoyingze.com>
2022-01-20 00:10:12 +0800

    fix port test

--- proxy/test.php
+++ proxy/test.php
@@ -1,7 +1,7 @@
 <?php
-echo $_SERVER["HTTPS"];
-echo isset($_SERVER["HTTPS"]);
-echo $_SERVER["SERVER_PORT"];
-echo $_SERVER["SERVER_PORT"] === 443;
-echo $_SERVER["SERVER_PORT"] == 443;
+echo $_SERVER["HTTPS"] . '<br/>';
+echo isset($_SERVER["HTTPS"]) . '<br/>';
+echo $_SERVER["SERVER_PORT"] . '<br/>';
+echo ($_SERVER["SERVER_PORT"] === 443) . '<br/>';
+echo ($_SERVER["SERVER_PORT"] == 443) . '<br/>' ;
 ?>
