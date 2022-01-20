ed92bf295521c8d6e045047310eb8000c6a3e996
zhaoyingze <zhaoyingze@maoyan.com>
2022-01-20 12:56:26 +0800

    add http_origin in test file

--- proxy/test.php
+++ proxy/test.php
@@ -4,4 +4,5 @@ echo isset($_SERVER["HTTPS"]) . '<br/>';
 echo $_SERVER["SERVER_PORT"] . '<br/>';
 echo ($_SERVER["SERVER_PORT"] === 443) . '<br/>';
 echo ($_SERVER["SERVER_PORT"] == 443) . '<br/>' ;
+echo $_SERVER["HTTP_ORIGIN"]  . '<br/>' ;
 ?>
